9887d2183fc324c2225582e7ec81871324a3cb05
Felix Hoffmann <felix.hoffmann2@student.uni-halle.de>
2019-06-15 01:34:58 +0200

    Added basic records to the main file

--- src/Main.elm
+++ src/Main.elm
@@ -9,6 +9,78 @@ type alias Model =
     { count : Int }
 
 
+
+-- Basic record for storing the information of the selected hero
+
+
+type alias Hero =
+    { name : String
+    , rarity : Int
+    , element : String
+    , stats : Stats
+    , skills : List Skill
+    }
+
+
+
+-- Basic record to hold the selected hero's stats
+
+
+type alias Stats =
+    { atk : Int -- Attack
+    , hp : Int -- Hitpoints
+    , spd : Int -- Speed
+    , def : Int -- Defense
+    , chc : Float -- Critical Hit Chance
+    , chd : Float -- Critical Hit Damage
+    , eff : Float -- Effectiveness
+    , efr : Float -- Effect Resistance
+    , dac : Float -- Dual Attack Chance
+    }
+
+
+
+-- Basic template for hero skills
+
+
+type alias Skill =
+    { name : String
+    , isPassive : Bool
+    , description : String
+    , cooldown : Int
+    , soulburn : Int --cost in souls, 0 means not soulburnable
+    , soulburnEffect : String
+    , modifiers : List Modifier --list of all modifiers of the skill
+    }
+
+
+
+-- Basic template for each possible modifier for a hero's skill
+
+
+type alias Modifier =
+    { category : String -- defines which stat this modifier scales of
+    , section : Section -- defines if the modifier is additive or multiplicative
+    , value : Float -- defines the magnitude of the modifier
+    }
+
+
+
+-- Basic template to define certain key enemies you fight very often in the game to measure your damage against those
+
+
+type alias Enemy =
+    { name : String
+    , element : String
+    , defValue : Int
+    }
+
+
+type Section
+    = Additive
+    | Multiplicative
+
+
 initialModel : Model
 initialModel =
     { count = 0 }
@@ -16,26 +88,18 @@ initialModel =
 
 type Msg
     = Increment
-    | Decrement
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
         Increment ->
-            { model | count = model.count + 1 }
-
-        Decrement ->
-            { model | count = model.count - 1 }
+            model
 
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ button [ onClick Increment ] [ text "+1" ]
-        , div [] [ text <| String.fromInt model.count ]
-        , button [ onClick Decrement ] [ text "-1" ]
-        ]
+    div [] []
 
 
 main : Program () Model Msg
